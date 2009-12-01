5edaacf46eab5b0fec8d0213fd389532b3a672b7
Havard Graff <havard.graff@tandberg.com>
2009-12-01 14:03:41 +0100

    Omit fatal MessageBox on Windows in Debug configuration

--- glib/gmessages.c
+++ glib/gmessages.c
@@ -539,10 +539,12 @@ g_logv (const gchar   *log_domain,
 	  if ((test_level & G_LOG_FLAG_FATAL) && !masquerade_fatal)
             {
 #ifdef G_OS_WIN32
+#ifndef _DEBUG
 	      gchar *locale_msg = g_locale_from_utf8 (fatal_msg_buf, -1, NULL, NULL, NULL);
 	      
 	      MessageBox (NULL, locale_msg, NULL,
 			  MB_ICONERROR|MB_SETFOREGROUND);
+#endif /* _DEBUG */
 	      if (IsDebuggerPresent () && !(test_level & G_LOG_FLAG_RECURSION))
 		G_BREAKPOINT ();
 	      else
